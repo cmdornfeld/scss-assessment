ebae4dd26ce6cd66c4fc025515817ee62e2a72c7
Chad Dornfeld <cmdornfeld@gmail.com>
2021-02-09 14:30:16 -0600

    added SingleSelect js to MultiSelectas boilerplate

--- src/MultiSelect/MultiSelect.js
+++ src/MultiSelect/MultiSelect.js
@@ -1,4 +1,4 @@
-import React, {} from 'react'
+import React, {useState, useEffect} from 'react'
 import './MultiSelect.scss'
 
 // this is a stub for you to develop the following
@@ -16,9 +16,46 @@ import './MultiSelect.scss'
 */
 
 const MultiSelect = props => {
+
+    const [selected, setSelected] = useState(-1);
+
+    const handleSelect = (i) => {
+        if(selected === -1) setSelected(i)
+    }
+
+    const selectedOption = props.data.options[selected]
+
     return (
         <div className={`MultiSelect`}>
-            multi-select implementation goes here
+            <h1>
+                {props.data.questionText}
+            </h1>
+            {
+                selected === -1 &&
+                props.data.options.map((option, optionIndex) => {
+                    return <button onClick={()=>{handleSelect(optionIndex)}}>{option.text}</button>
+                })
+            }
+            {
+                selected > -1 &&
+                <div className={`feedback ${selectedOption.correct ? 'correct' : 'incorrect'}`}>
+                    <h1>
+                        {selectedOption.correct ?
+                            props.data.feedback.correct.header
+                        :
+                            props.data.feedback.incorrect.header
+                        }
+                    </h1>
+                    <p>
+                        {selectedOption.correct ?
+                            props.data.feedback.correct.body
+                        :
+                            props.data.feedback.incorrect.body
+                        }
+                    </p>
+                    <button onClick={props.onComplete}>OK</button>
+                </div>
+            }
         </div>
     )
 }
